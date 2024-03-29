ab729eb12acc816e64c8819e062e188e000fc598
Maglys Maitae <mmaita83@gmail.com>
2024-03-29 17:12:25 -0300

    Se crean las Cardss donde se visualizan los datos correspondientes al Json.

--- byteworks-web/src/App.jsx
+++ byteworks-web/src/App.jsx
@@ -1,36 +1,27 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/home";
-import Login from "./pages/Login/login";
-import Register  from "./pages/Register/register";
-import Talent from "./pages/Talent/talent";
-import Enterprise from "./pages/Enterprise/enterprise";
 
-const App = () => {
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/home';
+import Login from './pages/Login/login';
+import Register from './pages/Register/register';
+import Talent from './pages/Talent/talent';
+import Enterprise from './pages/Enterprise/enterprise';
+import JobDetails from './pages/JobDetails/jobs';
+
+function App() {
+  
   return (
     <div>
       <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-        <Route
-          path="/talent"
-          element={<Talent />}
-        />
-        <Route
-          path="/enterprise"
-          element={<Enterprise />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/talent" element={<Talent />} />
+        <Route path="/enterprise" element={<Enterprise />} />
+        <Route path="/jobs" element={<JobDetails />} />
       </Routes>
     </div>
   );
-};
+}
+
 export default App;
