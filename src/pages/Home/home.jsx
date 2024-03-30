259824f25b55f495c3854a811800e4ea233696cf
JuanLemus07 <juanlemus10784@gmail.com>
2024-03-30 06:00:53 -0300

    agregando drawer responsivo a la navbar, diseno del home(no terminado),se elimino archivo y carpetas en desuso, se cambio el titulo y se agrego el logo en la nav y en el html.

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,39 @@
- import React from "react";
- 
- function Home(){
-    return <h1>Home</h1>;
- }
+// eslint-disable-next-line no-unused-vars
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
-export default Home
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function Home() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <Item><img src="/src/assets/img/img1.jpg" width="250px"  alt="" /></Item>
+        </Grid>
+        <Grid item xs={6}>
+        <Item><img src="/src/assets/img/img2.jpg" width="250px"  alt="" /></Item>
+        </Grid>
+        <Grid item xs={6}>
+        <Item><img src="/src/assets/img/img3.jpg" width="250px"  alt="" /></Item>
+        </Grid>
+        <Grid item xs={6}>
+        <Item><img src="/src/assets/img/img4.jpg" width="250px"  alt="" /></Item>
+        </Grid>
+      </Grid>
+      <Typography variant="h2" component="h4" textAlign="center">
+        We are create solutions for you
+      </Typography>
+    </Box>
+  );
+}
