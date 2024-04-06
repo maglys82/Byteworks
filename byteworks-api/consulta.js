aba04a93753fd5db974debea9eb3274e36410210
Maglys Maitae <mmaita83@gmail.com>
2024-04-06 20:47:20 -0300

    Se realizn las prubas de test

--- byteworks-api/consulta.js
+++ byteworks-api/consulta.js
@@ -11,4 +11,46 @@ const pool = new Pool({
     allowExitOnIdle: true
 })
 
-module.exports= {pool}
+
+// 1.-Funcion insertar en la tabla Jobs y usuarios
+const guardarJobs = async (name, business, type_of_service, status) => {
+    const consulta = "INSERT INTO jobs values (DEFAULT,$1.$2,$3,$4)";
+    const values = [name, business, type_of_service, status];
+    try {
+        const result = await pool.query(consulta, values);
+        return result;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+}
+
+const guardarUsuarios = async (name, rol, skill, review, status, comentario) => {
+    const consulta = "INSERT INTO usuarios values (DEFAULT,$1,$2,$3,$4,$5,$6)";
+    const values = [name, rol, skill, review, status, comentario];
+    try {
+        const result = await pool.query(consulta, values);
+        return result;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+}
+
+//Funcion Obtener todas los jobs  y usuarios
+const obtenerJobs = async () => {
+    const consulta = 'Select * from jobs'
+    const { rows } = await pool.query(consulta);
+    return rows;
+}
+
+const obtenerUsuarios = async () => {
+    const consulta = 'Select * from usuarios'
+    const { rows } = await pool.query(consulta);
+    return rows;
+}
+
+module.exports= {pool,guardarJobs,guardarUsuarios,obtenerJobs,obtenerUsuarios}
---
+++ byteworks-api/test/server.spec.js
@@ -0,0 +1,36 @@
+const request = require("supertest");
+const server = require("../server");
+
+
+describe('POST /jobs', () => {
+  it('debería devolver un JSON con datos de trabajos desde la base de datos', async () => {
+    const jobs = {
+      "name": "Desarrolador Frontend",
+      "business":"Consultora Conexiones",
+      "type_of_service": "Desarrollador",
+      "status":"En progreso"
+    }
+    // Realiza una solicitud a la ruta '/jobs'
+    const response = await request(server).post('/jobs').send(jobs);
+
+    // Verifica el código de estado
+    expect(response.status).toBe(201);
+
+    // Verifica que la respuesta contenga los datos obtenidos de la base de datos
+    expect(response.body).toEqual('Recurso agregado con Exito');
+  });
+
+  it('debería devolver un Error cuando envío datos invalidos', async () => {
+    const jobs = {
+      "name": "mmita83@gmail.com"
+    }
+    // Realiza una solicitud a la ruta '/jobs'
+    const response = await request(server).post('/jobs').send(jobs);
+
+    // Verifica el código de estado
+    expect(response.status).toBe(400);
+
+    // Verifica que la respuesta contenga los datos obtenidos de la base de datos
+    expect(response.body).toEqual('Faltan datos Requeridos');
+  });
+});
