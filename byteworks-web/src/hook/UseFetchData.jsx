e4f3eb657113496d7f795ad30be04f542c6a68f7
Gregoris Lemus <gregoris lemus>
2024-04-11 22:09:41 -0400

    fix url

--- byteworks-web/src/hook/UseFetchData.jsx
+++ byteworks-web/src/hook/UseFetchData.jsx
@@ -7,7 +7,7 @@ const UseFetchData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.mockfly.dev/mocks/c50a65da-18d7-4274-96c8-699ac98eb2c9/users');
+        const response = await axios.get('/users');
         const json = await response.data; // change response.json() to response.data
         setData(json);
       } catch (error) {
